c9f7ddb654b4fe15c5f2be13e08b3ca047b87f50
jodin22 <jeromekrwc@gmail.com>
2022-06-03 04:32:11 -0700

    test new variable, function, work in progress

--- javascript.js
+++ javascript.js
@@ -33,9 +33,21 @@ console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === "rock" && playerSelection === "scissors") {
-        console.log("")
+        console.log("You Lose! Rock beats scissors.");
+    } else if (computerSelection === "rock" && playerSelection === "paper") {
+        console.log("You Win! Paper beats rock.");
+    } else if (computerSelection === "paper" && playerSelection === "scissors") {
+        console.log("You Win! Scissors beats paper.");
+    } else if (computerSelection === "paper" && playerSelection === "rock") {
+        console.log("You Lose! Paper beats rock.");
+    } else if (computerSelection === "scissors" && playerSelection === "rock") {
+        console.log("You Win! Rock beats scissors.");
+    } else if (computerSelection === "scissors" && playerSelection === "paper") {
+        console.log("You Lose! Scissors beats paper.");
+    } else {
+        console.log("You tie! You chose the same as the computer");
     }
-
+      
 }
 
 playRound(playerSelection, computerSelection);
@@ -43,7 +55,16 @@ playRound(playerSelection, computerSelection);
 // if computerSelection is rock and playerSelection is rock, then show tie
 // if computerSelection is rock and playerSelection is scissors, then show computer wins
 // if computerSelection is rock and playerSelection is paper, then show player wins
+/*
+if computer is paper and player is scissors, then show you win
+if computer is paper and player is rock, then show you lose
+if computer is paper and player is paper, show tie
 
+if computer is scissors and player is rock, then show you win
+if computer is scissors and player is paper, then show you lose
+if computer is scissors and player is scissors, then show tie
+
+*/
 
 
 
