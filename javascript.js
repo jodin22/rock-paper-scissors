22dd98e8c933a796d3e48ccd4b4b2ed50107bde5
jodin22 <jeromekrwc@gmail.com>
2022-06-04 19:07:20 -0700

    Kept single round play the same. Put inside loop.

--- javascript.js
+++ javascript.js
@@ -5,6 +5,12 @@
 // alert ("Did the console.log work?");
 
 // the below is selecting rock paper or scissors by the computer but it will randomize the choice
+
+// line 80 was first attempt at game() and loop. didn't work. tried again with just the loop and kept 
+// the code for the single round the same (lines 14 to 74). wrapped the code for the single round inside a loop.
+// the loop works. next step is to do the game().
+
+for (let i = 0; i < 5; i++) {
 function computerPlay() {
     const choice = ['Rock', 'Paper', 'Scissors']; // the array of 3 choices. 0 Rock, 1 Paper, 2 Scissors.
     // for (const selection of selections) {
@@ -16,64 +22,62 @@ function computerPlay() {
 
     const randomChoice = choice[Math.floor(Math.random() * choice.length)]; // randomizes the array items  
     const computerSelection = randomChoice.toLowerCase(); // need lower case for comparison with player choice
-    console.log(computerSelection);
     return computerSelection; // to send the choice to another function or variable 
     
     // https://herewecode.io/blog/get-random-element-array-javascript/
 }
 
-// const computerSelection = computerPlay();  // puts the return of the function into a variable
+const computerSelection = computerPlay();  // puts the return of the function into a variable
 // console.log(computerPlay()); shows the computer choice but will not pass the value to the playRound
 // function.
-// console.log(computerSelection); // prints the value. maybe console.log(computerPlay()) is sufficient without
-// line 25? Not true. Must do it this way, line 25 and 28.
+console.log(computerSelection); // prints the value. maybe console.log(computerPlay()) is sufficient without
+// line 30? Not true. Must do it this way, line 30 and 33.
 
 // the below plays a single round. shows who the winner is. 
-// const playerInput = prompt("Please input Rock, Paper or Scissors: ");
-// const playerSelection = playerInput.toLowerCase(); // convert to lower case for comparison
-// console.log(playerSelection);
-
-game();
+const playerInput = prompt("Please input Rock, Paper or Scissors: ");
+const playerSelection = playerInput.toLowerCase(); // convert to lower case for comparison
+console.log(playerSelection);
 
 function playRound(playerSelection, computerSelection) {
     if (computerSelection === "rock" && playerSelection === "scissors") {
         let message = "You Lose! Rock beats scissors.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else if (computerSelection === "rock" && playerSelection === "paper") {
         let message = "You Win! Paper beats rock.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else if (computerSelection === "paper" && playerSelection === "scissors") {
         let message = "You Win! Scissors beats paper.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else if (computerSelection === "paper" && playerSelection === "rock") {
         let message = "You Lose! Paper beats rock.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else if (computerSelection === "scissors" && playerSelection === "rock") {
         let message = "You Win! Rock beats scissors.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else if (computerSelection === "scissors" && playerSelection === "paper") {
         let message = "You Lose! Scissors beats paper.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     } else {
         let message = "You tie! You chose the same as the computer.";
-        // return message;
-        console.log(message);
+        return message;
+        // console.log(message);
     }
       
 }
 
-// console.log(playRound(playerSelection, computerSelection)); // prints the return of the function which are 
+console.log(playRound(playerSelection, computerSelection)); // prints the return of the function which are 
 // messages
+}
 
 // game() calls the playRound() but from inside the game(). plays 5 rounds only so a loop for 5 times
 
-function game(computerSelection) { // seems to generate the computer and user input correctly, but 
+/* function game() { // seems to generate the computer and user input correctly, but 
     // it fails all the conditions and only shows the else part of the tie message. continue troubleshooting
     for (let i = 0; i < 5; i++) {
         computerPlay();
@@ -82,7 +86,7 @@ function game(computerSelection) { // seems to generate the computer and user in
         console.log(playerSelection);
         playRound(playerSelection, computerSelection);
     }
-}
+} */
 
 
 // if computerSelection is rock and playerSelection is rock, then show tie
