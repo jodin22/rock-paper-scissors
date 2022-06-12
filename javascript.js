ae3aabfff5a1a620793042adeef625a0927e5002
jodin22 <jeromekrwc@gmail.com>
2022-06-11 19:13:24 -0700

    Edit comments, more testing

--- javascript.js
+++ javascript.js
@@ -7,7 +7,7 @@
 // the below is selecting rock paper or scissors by the computer but it will randomize the choice
 
 function game() {
-    let computerScore = 0;
+    let computerScore = 0;  // track win total. starts at 0 and the loop will increase by 1 in each round
     let playerScore = 0;
 
     for (let i = 0; i < 5; i++) {
@@ -21,15 +21,16 @@ function game() {
 
             const randomChoice = choice[Math.floor(Math.random() * choice.length)]; // randomizes the array items  
             const computerSelection = randomChoice.toLowerCase(); // need lower case for comparison with player choice
-            return computerSelection; // to send the choice to another function or variable 
+            return computerSelection; // to send the choice to another function or variable
+            // if you don't use return, this value is not visible outside this function 
     
     // https://herewecode.io/blog/get-random-element-array-javascript/
         }
 
-        const computerSelection = computerPlay();  // puts the return of the function into a variable
+        const computerSelection = computerPlay();  // calls the function and puts the return of the function into a variable
         console.log("Computer shows: " + `${computerSelection}`); // prints the value. 
         
-// the below plays a single round. shows who the winner is. 
+// the below plays a single round (line 34 to 77). shows who the winner is and keeps score.
         const playerInput = prompt("Please input Rock, Paper or Scissors: ", "Scissors");
         const playerSelection = playerInput.toLowerCase(); // convert to lower case for comparison
         console.log("Player shows: " + `${playerSelection}`);
@@ -38,42 +39,42 @@ function game() {
             if (computerSelection === "rock" && playerSelection === "scissors") {
                 let message = "You Lose! Rock beats scissors.";
                 computerScore = 1 + computerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else if (computerSelection === "rock" && playerSelection === "paper") {
                 let message = "You Win! Paper beats rock.";
                 playerScore = 1 + playerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else if (computerSelection === "paper" && playerSelection === "scissors") {
                 let message = "You Win! Scissors beats paper.";
                 playerScore = 1 + playerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else if (computerSelection === "paper" && playerSelection === "rock") {
                 let message = "You Lose! Paper beats rock.";
                 computerScore = 1 + computerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else if (computerSelection === "scissors" && playerSelection === "rock") {
                 let message = "You Win! Rock beats scissors.";
                 playerScore = 1 + playerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else if (computerSelection === "scissors" && playerSelection === "paper") {
                 let message = "You Lose! Scissors beats paper.";
                 computerScore = 1 + computerScore;
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             } else {
                 let message = "You tie! You chose the same as the computer.";
-                return message + ` Computer win: ${computerScore}. Player win: ${playerScore}`;
+                return message + ` Computer win total: ${computerScore}. Player win total: ${playerScore}`;
         // console.log(message);
             }
       
         }
 
-console.log(playRound(playerSelection, computerSelection)); // prints the return of the function which are 
+console.log(playRound(playerSelection, computerSelection)); // calls the function and prints the return of the function which are 
 // messages
 
     }
